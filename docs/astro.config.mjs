fb1e4dcd296be46d1c00b85721a5e50355df321f
kiraaziz <kiraaziz2020@gmail.com>
2024-01-19 20:38:41 +0100

    add vercel

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mangahook-api.vercel.app",
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 	integrations: [
 		starlight({
 			title: 'Manga Hook',
