b828b15b609382d9abe3b8b3ed557f4dbc681b18
kiraaziz <kiraaziz2020@gmail.com>
2024-01-19 22:06:37 +0100

    add to vercel

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'
 import starlight from '@astrojs/starlight';
 import vercelStatic from '@astrojs/vercel/static';
 // https://astro.build/config
